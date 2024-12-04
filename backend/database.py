c113eed9db7beb65c094d5b8d72af6e5849aa725
JH <0john.hong0@gmail.com>
2024-12-05 00:07:11 +0900

    Modify-database url

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost/chat_db"
+# DATABASE_URL = "mysql+pymysql://root:root@localhost/chat_db"
+DATABASE_URL = "mysql+pymysql://db_project:db_project!@192.168.0.118:3307/DBproject"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
